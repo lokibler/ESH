956d60326dfb165db6d07009c81c694f2516404d
lokibler <lokibler@umich.edu>
2025-04-06 15:47:20 -0700

    trying npoint for json

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ let currentLocation = null;
 let stream = null;
 let photoCanvas = null;
 let photoContext = null;
-let spreadsheetId = null;
 
 // Configuration
-const SPREADSHEET_ID = '1f-sxasESzH058TjfNwaIF7b35u1RvG3AQaCFrVAFmKk'; // Replace with your spreadsheet ID
 const GOOGLE_DRIVE_FOLDER_ID = '1fSH2Xfeb1LT-PnpFkZE-SRDgkD-lqGRj'; // For storing photos
-const SHEET_NAME = 'Teams';
+const NPOINT_ID = '7d8a2bb0fe5092349a03'; // Replace with your npoint.io bin ID
 const CLIENT_ID = '770657216624-v7j2d3bdpsj2t70qejqiselj5u077h2u.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyDxR99_WeVcr4mA8AmalaJ85VlqdI7oocs';
 
@@ -33,29 +31,6 @@ function storeToken(token, expiresIn) {
     localStorage.setItem('googleDriveToken', JSON.stringify({ token, expiresAt }));
 }
 
-// Initialize Google API
-async function initializeGoogleAPI() {
-    try {
-        await gapi.client.init({
-            apiKey: API_KEY,
-            discoveryDocs: [
-                'https://sheets.googleapis.com/$discovery/rest?version=v4',
-                'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
-            ],
-        });
-        
-        window.tokenClient = google.accounts.oauth2.initTokenClient({
-            client_id: CLIENT_ID,
-            scope: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive',
-            callback: '', // defined later
-        });
-        
-        console.log('Google API initialized successfully');
-    } catch (error) {
-        console.error('Error initializing Google API:', error);
-    }
-}
-
 // Get a valid token
 async function getValidToken() {
     // Check for stored token first
@@ -75,234 +50,51 @@ async function getValidToken() {
             storeToken(resp.access_token, resp.expires_in);
             resolve(resp.access_token);
         };
-        window.tokenClient.requestAccessToken({prompt: ''});
+        window.tokenClient.requestAccessToken({prompt: 'consent'});
     });
 }
 
-// Tasks data organized by location
-const tasks = {
-    'UK/Canada': [
-        { id: 'uk1', description: 'With a Winnie the Pooh character topiary', points: 3 },
-        { id: 'uk2', description: 'With Tinkerbell topiary', points: 3 },
-        { id: 'uk3', description: 'In/by a Red Phone booth', points: 3 },
-        { id: 'uk4', description: 'Find your way out of the hedge maze', points: 3 },
-        { id: 'uk5', description: 'Giant tea pot tea displays', points: 3 },
-        { id: 'uk6', description: 'Pretend drinking tea in the tea store', points: 3 },
-        { id: 'uk7', description: 'Looking in the window at winnie the pooh plush', points: 3 },
-        { id: 'uk8', description: 'Sitting at water fountain', points: 3 },
-        { id: 'uk9', description: 'Ship in a bottle', points: 4 },
-        { id: 'uk10', description: 'The knights in shining armor', points: 3 },
-        { id: 'uk11', description: 'With the redcoat statue at attention', points: 3 },
-        { id: 'ca1', description: 'The big waterfall', points: 3 },
-        { id: 'ca2', description: 'Flying with the geese topiaries', points: 3 },
-        { id: 'ca3', description: 'A totem pole', points: 2 },
-        { id: 'ca4', description: 'With Canadian flag', points: 3 },
-        { id: 'ca5', description: '"Drinking" maple syrup', points: 3 },
-        { id: 'ca6', description: 'In the photo stand-in', points: 2 },
-        // Common tasks for UK/Canada
-        { id: 'ukca1', description: 'Picture with a cast member in any pavilion', points: 4 },
-        { id: 'ukca2', description: 'Posing as and with a topiary', points: 2 },
-        { id: 'ukca3', description: 'Surprised by a wait time over 50 minutes', points: 3 },
-        { id: 'ukca4', description: 'Something over $1,000', points: 4 },
-        { id: 'ukca5', description: 'Dancing to a band playing live music', points: 3 },
-        { id: 'ukca6', description: 'Food packaging in a different language', points: 3 },
-        { id: 'ukca7', description: 'Hidden mickey', points: 4 }
-    ],
-    'Mexico/Norway/China': [
-        { id: 'mx1', description: 'In a sombrero', points: 3 },
-        { id: 'mx2', description: 'Sitting on Mexico indoor fountain', points: 3 },
-        { id: 'mx3', description: 'Pointing at plaza de los amigos sign', points: 3 },
-        { id: 'mx4', description: 'Swooning over everlasting love statue', points: 3 },
-        { id: 'mx5', description: 'Something with the three caballeros on it', points: 3 },
-        { id: 'mx6', description: 'In front of the pre-Columbian pyramid', points: 2 },
-        { id: 'no1', description: 'Wooden sculpture of Kristoff', points: 3 },
-        { id: 'no2', description: 'Statue of Norwegian marathon champion Grete Waitz', points: 4 },
-        { id: 'no3', description: 'Royal Commerhaus sign', points: 3 },
-        { id: 'no4', description: 'Stave Church', points: 2 },
-        { id: 'no5', description: 'King Olaf II Haroldson of Norway Statue', points: 3 },
-        { id: 'no6', description: 'Image of thor in the Gallery', points: 3 },
-        { id: 'no7', description: 'Image of the goddess of love in the Gallery', points: 4 },
-        { id: 'no8', description: 'Something Frozen themed', points: 2 },
-        { id: 'no9', description: 'With the troll', points: 4 },
-        { id: 'cn1', description: 'Take a picture with Leigh\'s chinese zodiac symbol', points: 4 },
-        { id: 'cn2', description: 'Gardens of imagination map', points: 4 },
-        { id: 'cn3', description: 'Something with a dragon on it', points: 3 },
-        { id: 'cn4', description: 'A buddha', points: 3 },
-        { id: 'cn5', description: 'A lucky cat', points: 3 },
-        { id: 'cn6', description: 'Under a chinese parasol umbrella', points: 3 },
-        { id: 'cn7', description: 'One of the lion statues protecting the house of good fortune', points: 3 },
-        { id: 'cn8', description: 'Hiding behind a china folding fan', points: 2 },
-        { id: 'cn9', description: 'In front of the bianzhong', points: 3 },
-        { id: 'cn10', description: 'With the small elephant statue', points: 3 },
-        { id: 'cn11', description: 'In front of the temple of heaven', points: 2 },
-        { id: 'cn12', description: 'With the zootopia art', points: 3 },
-        // Common tasks for Mexico/Norway/China
-        { id: 'mnc1', description: 'Picture with a cast member in any pavilion', points: 4 },
-        { id: 'mnc2', description: 'Posing as and with a topiary', points: 2 },
-        { id: 'mnc3', description: 'Surprised by a wait time over 50 minutes', points: 3 },
-        { id: 'mnc4', description: 'Something over $1,000', points: 4 },
-        { id: 'mnc5', description: 'Dancing to a band playing live music', points: 3 },
-        { id: 'mnc6', description: 'Food packaging in a different language', points: 3 },
-        { id: 'mnc7', description: 'Hidden mickey', points: 4 }
-    ],
-    'Germany/Italy/US': [
-        { id: 'de1', description: 'Doors to Rhine River Cruise attraction', points: 4 },
-        { id: 'de2', description: 'Miniature Train and Village', points: 2 },
-        { id: 'de3', description: 'Pointing at miniature flower and garden banner', points: 3 },
-        { id: 'de4', description: 'Hugging a Steiff plush', points: 3 },
-        { id: 'de5', description: 'Posing like the clock hands with the clock tower', points: 2 },
-        { id: 'de6', description: 'Swooning for the knight in the fountain', points: 3 },
-        { id: 'de7', description: '"Cheers!" with a stein', points: 3 },
-        { id: 'de8', description: 'Cuckoo clock', points: 3 },
-        { id: 'de9', description: 'Wowww at the crystal cinderella\'s castle/spaceship earth/large crystal statue', points: 3 },
-        { id: 'de10', description: 'Large golden nutcracker', points: 3 },
-        { id: 'it1', description: 'In front of St Mark\'s Campanile', points: 2 },
-        { id: 'it2', description: 'Sitting on the neptune fountain', points: 2 },
-        { id: 'it3', description: 'With the parked gondolas', points: 2 },
-        { id: 'it4', description: 'The donkey', points: 3 },
-        { id: 'it5', description: '"Using" fancy perfume', points: 3 },
-        { id: 'it6', description: 'By the moped', points: 3 },
-        { id: 'it7', description: 'Something with a ferrari logo', points: 3 },
-        { id: 'it8', description: 'Licking lips at gelateria', points: 3 },
-        // Common tasks for Germany/Italy/US
-        { id: 'deit1', description: 'Picture with a cast member in any pavilion', points: 4 },
-        { id: 'deit2', description: 'Posing as and with a topiary', points: 2 },
-        { id: 'deit3', description: 'Surprised by a wait time over 50 minutes', points: 3 },
-        { id: 'deit4', description: 'Something over $1,000', points: 4 },
-        { id: 'deit5', description: 'Dancing to a band playing live music', points: 3 },
-        { id: 'deit6', description: 'Food packaging in a different language', points: 3 },
-        { id: 'deit7', description: 'Hidden mickey', points: 4 }
-    ],
-    'Japan/Morocco/France': [
-        { id: 'jp1', description: 'Tree pose with a bonsai tree', points: 3 },
-        { id: 'jp2', description: 'Hugging a Studio Ghibli film character plush', points: 3 },
-        { id: 'jp3', description: 'Hugging a pokemon plush', points: 3 },
-        { id: 'jp4', description: 'Fish face with koi pond in Japan', points: 3 },
-        { id: 'jp5', description: 'Watching a water feature', points: 2 },
-        { id: 'jp6', description: 'Winged statue in kokedama garden', points: 3 },
-        { id: 'jp7', description: 'A lucky cat', points: 3 },
-        { id: 'jp8', description: 'Duffy and friends plushies in the gallery', points: 3 },
-        { id: 'jp9', description: 'Kawaii museum harajuku girl', points: 3 },
-        { id: 'jp10', description: 'Warrior on horse statue', points: 2 },
-        { id: 'jp11', description: 'In front of the arch in the water', points: 2 },
-        { id: 'fr1', description: 'Plugging your nose by a Remy sewer grate', points: 2 },
-        { id: 'fr2', description: 'Health inspector plaque', points: 4 },
-        { id: 'fr3', description: 'Excited for Château d\'Ego wines', points: 4 },
-        { id: 'fr4', description: 'Reading in front of the book case', points: 3 },
-        { id: 'fr5', description: 'Maleficent\'s outfit display', points: 3 },
-        { id: 'fr6', description: 'Ratatouille fountain', points: 3 },
-        { id: 'fr7', description: 'Woman by bicycle display', points: 3 },
-        { id: 'mo1', description: 'Competing in "rally of the gazelles"', points: 3 },
-        // Common tasks for Japan/Morocco/France
-        { id: 'jmf1', description: 'Picture with a cast member in any pavilion', points: 4 },
-        { id: 'jmf2', description: 'Posing as and with a topiary', points: 2 },
-        { id: 'jmf3', description: 'Surprised by a wait time over 50 minutes', points: 3 },
-        { id: 'jmf4', description: 'Something over $1,000', points: 4 },
-        { id: 'jmf5', description: 'Dancing to a band playing live music', points: 3 },
-        { id: 'jmf6', description: 'Food packaging in a different language', points: 3 },
-        { id: 'jmf7', description: 'Hidden mickey', points: 4 }
-    ]
-};
+// Initialize Google API
+async function initializeGoogleAPI() {
+    try {
+        await gapi.client.init({
+            apiKey: API_KEY,
+            discoveryDocs: [
+                'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
+            ],
+        });
+        
+        window.tokenClient = google.accounts.oauth2.initTokenClient({
+            client_id: CLIENT_ID,
+            scope: 'https://www.googleapis.com/auth/drive.file',
+            callback: '', // defined later
+        });
+        
+        // Try to get a token immediately to prompt for login at start
+        try {
+            await getValidToken();
+        } catch (error) {
+            console.error('Initial token fetch failed:', error);
+        }
+        
+        console.log('Google API initialized successfully');
+    } catch (error) {
+        console.error('Error initializing Google API:', error);
+    }
+}
 
-// Load team data from Google Sheets
+// Load team data from npoint.io
 async function loadTeam(teamName) {
     try {
         console.log('=== DEBUG: Starting loadTeam function ===');
-        console.log('Using spreadsheet ID:', SPREADSHEET_ID);
         
-        // Get valid token
-        const token = await getValidToken();
-        console.log('Got valid token');
-
-        // First, check if the sheet exists and create it if it doesn't
-        if (!spreadsheetId) {
-            try {
-                // Try to access the sheet
-                const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}?key=${API_KEY}`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'Referer': window.location.origin
-                    }
-                });
-
-                if (!response.ok) {
-                    // If sheet doesn't exist, create it
-                    console.log('Creating new spreadsheet...');
-                    const createResponse = await fetch('https://sheets.googleapis.com/v4/spreadsheets', {
-                        method: 'POST',
-                        headers: {
-                            'Authorization': `Bearer ${token}`,
-                            'Content-Type': 'application/json',
-                            'Referer': window.location.origin
-                        },
-                        body: JSON.stringify({
-                            properties: {
-                                title: 'ESH Teams'
-                            },
-                            sheets: [{
-                                properties: {
-                                    title: SHEET_NAME
-                                }
-                            }]
-                        })
-                    });
-
-                    if (!createResponse.ok) {
-                        throw new Error('Failed to create spreadsheet');
-                    }
-
-                    const createData = await createResponse.json();
-                    spreadsheetId = createData.spreadsheetId;
-                    
-                    // Initialize the sheet with headers
-                    await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${SHEET_NAME}!A1:C1?valueInputOption=RAW&key=${API_KEY}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Authorization': `Bearer ${token}`,
-                            'Content-Type': 'application/json',
-                            'Referer': window.location.origin
-                        },
-                        body: JSON.stringify({
-                            values: [['Team Name', 'Points', 'Completed Tasks']]
-                        })
-                    });
-                } else {
-                    spreadsheetId = SPREADSHEET_ID;
-                }
-            } catch (error) {
-                console.error('Error with spreadsheet:', error);
-                throw error;
-            }
-        }
-
-        // Get all data from the sheet
-        const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${SHEET_NAME}!A:C?key=${API_KEY}`, {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Referer': window.location.origin
-            }
-        });
-
+        const response = await fetch(`https://api.npoint.io/${NPOINT_ID}`);
         if (!response.ok) {
-            throw new Error('Failed to fetch sheet data');
+            throw new Error('Failed to fetch team data');
         }
 
         const data = await response.json();
-        console.log('Sheet data:', JSON.stringify(data, null, 2));
-
-        // Convert sheet data to teams object
-        const teams = {};
-        if (data.values && data.values.length > 1) { // Skip header row
-            data.values.slice(1).forEach(row => {
-                if (row[0]) { // If team name exists
-                    teams[row[0]] = {
-                        points: parseInt(row[1]) || 0,
-                        completedTasks: row[2] ? row[2].split(',') : []
-                    };
-                }
-            });
-        }
-
+        const teams = data.teams || {};
         console.log('Teams data:', JSON.stringify(teams, null, 2));
 
         // If no team name provided, return all teams data
@@ -319,64 +111,36 @@ async function loadTeam(teamName) {
     }
 }
 
-// Save team data to Google Sheets
+// Save team data to npoint.io
 async function saveTeam(teamName, teamData) {
     try {
         console.log('Starting saveTeam function...');
         console.log('Team name:', teamName);
         console.log('Team data:', JSON.stringify(teamData, null, 2));
 
-        // Get valid token
-        const token = await getValidToken();
-
-        // Get current data
-        const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${SHEET_NAME}!A:C?key=${API_KEY}`, {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Referer': window.location.origin
-            }
-        });
-
+        // Get current data first
+        const response = await fetch(`https://api.npoint.io/${NPOINT_ID}`);
         if (!response.ok) {
             throw new Error('Failed to fetch current data');
         }
 
         const data = await response.json();
-        let values = data.values || [['Team Name', 'Points', 'Completed Tasks']];
+        const teams = data.teams || {};
         
-        // Find if team already exists
-        const teamIndex = values.findIndex(row => row[0] === teamName);
-        
-        // Prepare the new row
-        const newRow = [
-            teamName,
-            teamData.points.toString(),
-            teamData.completedTasks.join(',')
-        ];
+        // Update team data
+        teams[teamName] = teamData;
 
-        if (teamIndex === -1) {
-            // Add new team
-            values.push(newRow);
-        } else {
-            // Update existing team
-            values[teamIndex] = newRow;
-        }
-
-        // Update the sheet
-        const updateResponse = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${SHEET_NAME}!A:C?valueInputOption=RAW&key=${API_KEY}`, {
-            method: 'PUT',
+        // Save back to npoint.io
+        const updateResponse = await fetch(`https://api.npoint.io/${NPOINT_ID}`, {
+            method: 'POST',
             headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-                'Referer': window.location.origin
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                values: values
-            })
+            body: JSON.stringify({ teams })
         });
 
         if (!updateResponse.ok) {
-            throw new Error('Failed to update sheet');
+            throw new Error('Failed to update team data');
         }
 
         return true;
